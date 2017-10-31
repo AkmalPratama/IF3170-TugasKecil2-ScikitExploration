f1c2618d5d2f29184ddc3d86c8059678e32d04a1
Akmal <m.akmalpratama@yahoo.co.id>
2017-10-31 23:52:54 +0700

    remove unused dataset iris, add kNN full, 90-10 training

--- explore_scikit.py
+++ explore_scikit.py
@@ -1,4 +1,6 @@
-from sklearn import datasets
+import numpy as np
+
+from sklearn import datasets, neighbors
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
@@ -8,25 +10,36 @@ iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=4)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.9, test_size=0.1, random_state=4)
 
-# kNN
-### Full training
-from sklearn.neighbors import KNeighborsClassifier
+### kNN full training
+print("\n")
+print("kNN full training")
+print("=================")
 knn = KNeighborsClassifier()
-knn.fit(X, y)
-y_pred = knn.predict(X)
+y_pred = knn.fit(X, y).predict(X)
+print("Number of mislabeled points out of a total %d points : %d" % (iris.data.shape[0], (iris.target != y_pred).sum()))
 
-acc = metrics.accuracy_score(y, y_pred)
-print('Training result %f' % (acc))
-
-#### Split training
+#### kNN 90% train, 10% test
+print("\n")
+print("kNN 90% train, 10% test")
+print("=======================")
 knn = KNeighborsClassifier()
-knn.fit(X_train, y_train)
-y_pred = knn.predict(X_test)
+y_pred = knn.fit(X_train, y_train).predict(X_test)
+print("Number of mislabeled points out of a total %d points : %d" % (len(X_test), (y_test != y_pred).sum()))
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, y_pred)
+np.set_printoptions(precision=2)
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix, without normalization')
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                      title='Normalized confusion matrix')
+plt.show()
 
-acc = metrics.accuracy_score(y_test, y_pred)
-print('Training result %f' % (acc))
 
 ### 10-fold
 
