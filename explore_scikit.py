69deacd53b6a63103f44a4df669471eee3b5b95f
Akmal <m.akmalpratama@yahoo.co.id>
2017-11-02 21:54:03 +0700

    add mlp training and confusion matrix plot

--- explore_scikit.py
+++ explore_scikit.py
@@ -1,16 +1,61 @@
+import itertools
 import numpy as np
+import matplotlib.pyplot as plt
 
-from sklearn import datasets, neighbors
-from sklearn import metrics
+from sklearn import datasets, svm
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 
 iris = datasets.load_iris()
 
 X = iris.data
 y = iris.target
+class_names = iris.target_names
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.9, test_size=0.1, random_state=4)
+
+# Run classifier, using a model that is too regularized (C too low) to see
+# the impact on the results
+classifier = svm.SVC(kernel='linear', C=0.01)
+y_pred = classifier.fit(X_train, y_train).predict(X_test)
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 ### kNN full training
 print("\n")
@@ -20,7 +65,7 @@ knn = KNeighborsClassifier()
 y_pred = knn.fit(X, y).predict(X)
 print("Number of mislabeled points out of a total %d points : %d" % (iris.data.shape[0], (iris.target != y_pred).sum()))
 
-#### kNN 90% train, 10% test
+### kNN 90% train, 10% test
 print("\n")
 print("kNN 90% train, 10% test")
 print("=======================")
@@ -64,15 +109,32 @@ print("Sum of mislabeled points : %d" % temp)
 print("Mean of mislabeled points : %.4f" % float(temp/10))
 
 # MLP
-### Full training
-from sklearn.neural_network import MLPClassifier
+### MLP full training
+print("\n")
+print("MLP full training")
+print("=================")
 mlp = MLPClassifier(hidden_layer_sizes=(10),solver='sgd',learning_rate_init=0.01,max_iter=500)
-mlp.fit(X, y)
+y_pred = mlp.fit(X, y).predict(X)
+print("Number of mislabeled points out of a total %d points : %d" % (iris.data.shape[0], (iris.target != y_pred).sum()))
 
-print(mlp.score(X_test,y_test))
+### MLP 90% train, 10% test
+print("\n")
+print("MLP 90% train, 10% test")
+print("=======================")
+#mlp = MLPClassifier(hidden_layer_sizes=(10),solver='sgd',learning_rate_init=0.01,max_iter=500, shuffle=False)
+mlp = MLPClassifier()
+y_pred = mlp.fit(X_train, y_train).predict(X_test)
+print("Number of mislabeled points out of a total %d points : %d" % (len(X_test), (y_test != y_pred).sum()))
 
-### Split training
-mlp = MLPClassifier(hidden_layer_sizes=(10),solver='sgd',learning_rate_init=0.01,max_iter=500)
-mlp.fit(X_train, y_train)
-
-print(mlp.score(X_test,y_test))
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, y_pred)
+np.set_printoptions(precision=2)
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix, without normalization')
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                      title='Normalized confusion matrix')
+plt.show()
